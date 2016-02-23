16991caae9c9639e51b4071028bea952eaf02982
mgoldade <marcigoldade@gmail.com>
2016-02-23 13:07:20 -0500

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,7 @@ Labels1<-read.csv("features.csv",header=FALSE)
 for (i in 1:nrow(Labels1)) {
   Step1<-as.matrix(str_trim(gsub("^[0-9][0-9]?[0-9]?","",str_trim(Labels1$V1))))
 }
-
+library(stringr)
 test_set<-read.csv("X_test.csv",header=FALSE)
 train_set<-read.csv("X_train.csv",header=FALSE)
 merge_data<-rbind(test_set,train_set)
@@ -27,19 +27,37 @@ Trial_Data$V1<-gsub("4","Sitting",Trial_Data$V1)
 Trial_Data$V1<-gsub("5","Standing",Trial_Data$V1)
 Trial_Data$V1<-gsub("6","Laying",Trial_Data$V1)
 
-colnames(Trial_Data)<-(c("Activity","TimeBodyAccelerationMagnitudeMean","TimeGravityAccelerationMagnitudeMean","TimeBodyAccelerationJerkMagnitudeMean","TimeBodyGyroscopeMagnitudeMean","TimeBodyGyroscopeJerkMagnitudeMean","FFTBodyAccelerationMagnitudeMean","FFTBodyBodyAccelerationJerkMagnitudeMean","FFTBodyBodyGyroscopeMagnitudeMean","FFTBodyBodyGyroscopeJerkMagnitudeMean","TimeBodyAccelerationMagnitudeStd","TimeGravityAccelerationMagnitudeStd","TimeBodyAccelerationJerkMagnitudeStd","TimeBodyGyroscopeMagnitudeStd","TimeBodyGyroscopeJerkMagnitudeStd","FFTBodyAccelerationMagnitudeStd","FFTBodyBodyAccelerationJerkMagnitudeStd","FFTBodyBodyGyroscopeMagnitudeStd","FFTBodyBodyGyroscopeJerkMagnitudeStd"))
+colnames(Trial_Data)<-(c("Activity","TimeBodyAccelerationMagnitudeMean",
+                                    "TimeGravityAccelerationMagnitudeMean",
+                                    "TimeBodyAccelerationJerkMagnitudeMean",
+                                    "TimeBodyAngularVelocityMagnitudeMean",
+                                    "TimeBodyAngularVelocityJerkMagnitudeMean",
+                                    "FreqBodyAccelerationMagnitudeMean",
+                                    "FreqBodyAccelerationJerkMagnitudeMean",
+                                    "FreqBodyAngularVelocityMagnitudeMean",
+                                    "FreqBodyAngularVelocityJerkMagnitudeMean",
+                                    "TimeBodyAccelerationMagnitudeStd",
+                                    "TimeGravityAccelerationMagnitudeStd",
+                                    "TimeBodyAccelerationJerkMagnitudeStd",
+                                    "TimeBodyAngularVelocityMagnitudeStd",
+                                    "TimeBodyAngularVelocityJerkMagnitudeStd",
+                                    "FreqBodyAccelerationMagnitudeStd",
+                                    "FreqBodyAccelerationJerkMagnitudeStd",
+                                    "FreqBodyAngularVelocityMagnitudeStd",
+                                    "FreqBodyAngularVelocityJerkMagnitudeStd"))
 
 Average_Data<-Trial_Data
 
+library(dplyr)
 Average_Data<-mutate(Average_Data,MeanofTimeBodyAccelerationMagnitudeMean=mean(TimeBodyAccelerationMagnitudeMean),
                      MeanofTimeGravityAccelerationMagnitudeMean=mean(TimeGravityAccelerationMagnitudeMean),MeanofTimeBodyAccelerationJerkMagnitudeMean=mean(TimeBodyAccelerationJerkMagnitudeMean),
-                     MeanofTimeBodyGyroscopeMagnitudeMean=mean(TimeBodyGyroscopeMagnitudeMean),MeanofTimeBodyGyroscopeJerkMagnitudeMean=mean(TimeBodyGyroscopeJerkMagnitudeMean),
-                     MeanofFFTBodyAccelerationMagnitudeMean=mean(FFTBodyAccelerationMagnitudeMean),MeanofFFTBodyBodyAccelerationJerkMagnitudeMean=mean(FFTBodyBodyAccelerationJerkMagnitudeMean),
-                     MeanofFFTBodyBodyGyroscopeMagnitudeMean=mean(FFTBodyBodyGyroscopeMagnitudeMean),MeanofFFTBodyBodyGyroscopeJerkMagnitudeMean=mean(FFTBodyBodyGyroscopeJerkMagnitudeMean),
+                     MeanofTimeBodyAngularVelocityMagnitudeMean=mean(TimeBodyAngularVelocityMagnitudeMean),MeanofTimeBodyAngularVelocityJerkMagnitudeMean=mean(TimeBodyAngularVelocityJerkMagnitudeMean),
+                     MeanofFreqBodyAccelerationMagnitudeMean=mean(FreqBodyAccelerationMagnitudeMean),MeanofFreqBodyAccelerationJerkMagnitudeMean=mean(FreqBodyAccelerationJerkMagnitudeMean),
+                     MeanofFreqBodyAngularVelocityMagnitudeMean=mean(FreqBodyAngularVelocityMagnitudeMean),MeanofFreqBodyAngularVelocityJerkMagnitudeMean=mean(FreqBodyAngularVelocityJerkMagnitudeMean),
                      MeanofTimeBodyAccelerationMagnitudeStd=mean(TimeBodyAccelerationMagnitudeStd),MeanofTimeGravityAccelerationMagnitudeStd=mean(TimeGravityAccelerationMagnitudeStd),
-                     MeanofTimeBodyAccelerationJerkMagnitudeStd=mean(TimeBodyAccelerationJerkMagnitudeStd),MeanofTimeBodyGyroscopeMagnitudeStd=mean(TimeBodyGyroscopeMagnitudeStd),
-                     MeanofTimeBodyGyroscopeJerkMagnitudeStd=mean(TimeBodyGyroscopeJerkMagnitudeStd),MeanofFFTBodyAccelerationMagnitudeStd=mean(FFTBodyAccelerationMagnitudeStd),
-                     MeanofFFTBodyBodyAccelerationJerkMagnitudeStd=mean(FFTBodyBodyAccelerationJerkMagnitudeStd),MeanofFFTBodyBodyGyroscopeMagnitudeStd=mean(FFTBodyBodyGyroscopeMagnitudeStd),
-                     MeanofFFTBodyBodyGyroscopeJerkMagnitudeStd=mean(FFTBodyBodyGyroscopeJerkMagnitudeStd))
+                     MeanofTimeBodyAccelerationJerkMagnitudeStd=mean(TimeBodyAccelerationJerkMagnitudeStd),MeanofTimeBodyAngularVelocityMagnitudeStd=mean(TimeBodyAngularVelocityMagnitudeStd),
+                     MeanofTimeBodyAngularVelocityJerkMagnitudeStd=mean(TimeBodyAngularVelocityJerkMagnitudeStd),MeanofFreqBodyAccelerationMagnitudeStd=mean(FreqBodyAccelerationMagnitudeStd),
+                     MeanofFreqBodyAccelerationJerkMagnitudeStd=mean(FreqBodyAccelerationJerkMagnitudeStd),MeanofFreqBodyAngularVelocityMagnitudeStd=mean(FreqBodyAngularVelocityMagnitudeStd),
+                     MeanofFreqBodyAngularVelocityJerkMagnitudeStd=mean(FreqBodyAngularVelocityJerkMagnitudeStd))
 
 write.table(Average_Data,"Average_Data.txt",row.names=FALSE) 
